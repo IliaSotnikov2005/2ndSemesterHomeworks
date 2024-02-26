98aa5d508c46d7871880e8ddbe23301ae6a8a17d
ilia Sotnikov <ilia12st@yandex.ru>
2024-02-26 17:34:30 +0300

    write trie class realisation

--- Trie/Program.cs
+++ Trie/Program.cs
@@ -1,17 +1,19 @@
 ﻿using System.Drawing;
 using System.Reflection.PortableExecutable;
+using static System.Net.Mime.MediaTypeNames;
 public class Trie
 {
     private class Vertex
     {
-        char tag;
-        Dictionary<char, Vertex> childs;
-        bool isTerminal;
+        private Dictionary<char, Vertex> childs;
+        private bool isTerminal;
+        private int countOfTerminals;
 
         public Vertex()
         {
             childs = new Dictionary<char, Vertex>();
             isTerminal = false;
+            countOfTerminals = 0;
         }
 
         public bool BuildNext(string text)
@@ -24,26 +26,89 @@ public class Trie
                 }
 
                 isTerminal = true;
+                countOfTerminals++;
                 return true;
             }
-            if (childs.ContainsKey(text[0]))
-            {
-                return childs[text[0]].BuildNext(text[1..]);
-            }
-            else
+
+            if (!childs.ContainsKey(text[0]))
             {
                 childs.Add(text[0], new Vertex());
-                return childs[text[0]].BuildNext(text[1..]);
             }
+
+            if (childs[text[0]].BuildNext(text[1..]))
+            {
+                countOfTerminals++;
+                return true;
+            }
+
+            return false;
+        }
+        public bool Find(string text)
+        {
+            if (text.Length == 0)
+            {
+                return isTerminal;
+            }
+            if (!childs.ContainsKey(text[0]))
+            {
+                return false;
+            }
+
+            return childs[text[0]].Find(text[1..]);
+        }
+        public bool Remove(string text)
+        {
+            if (text.Length == 0)
+            {
+                if (!isTerminal)
+                {
+                    return false;
+                }
+                countOfTerminals--;
+                isTerminal = false;
+                return true;
+            }
+            if (!childs.ContainsKey(text[0]))
+            {
+                return false;
+            }
+
+            if (childs[text[0]].Remove(text[1..]))
+            {
+                countOfTerminals--;
+                if (childs[text[0]].CountOfTerminals == 0)
+                {
+                    countOfTerminals--;
+                    childs.Remove(text[0]);
+                }
+                return true;
+            }
+
+            return false;
+        }
+        public int FindCountStartsWithPrefix(string prefix)
+        {
+            if (prefix.Length == 0)
+            {
+                return countOfTerminals;
+            }
+            if (!childs.ContainsKey(prefix[0]))
+            {
+                return 0;
+            }
+
+            return childs[prefix[0]].FindCountStartsWithPrefix(prefix[1..]);
+        }
+        public int CountOfTerminals
+        {
+            get { return countOfTerminals; }
         }
     }
 
-    private int size;
     private Vertex root;
 
     public Trie()
     {
-        size = 0;
         root = new Vertex();
     }
     public bool Add(string element)
@@ -51,14 +116,21 @@ public class Trie
         return root.BuildNext(element);
     }
     public bool Contains(string element)
-    { return true; }
-    public bool Remove(string element) { return true; }
+    {
+        return root.Find(element);
+    }
+    public bool Remove(string element)
+    {
+        return root.Remove(element);
+    }
     public int HowManyStartsWithPrefix(string prefix)
     {
-        return 0;
+        return root.FindCountStartsWithPrefix(prefix);
     }
-    int Size
-    { get { return size; } }
+    public int Size
+    {
+        get { return root.CountOfTerminals; }
+    }
 }
 
 public static class Program
@@ -66,6 +138,19 @@ public static class Program
     public static void Main()
     {
         Trie trie = new Trie();
-        trie.Add("hui");
+        Console.WriteLine(trie.Add("huita"));
+        Console.WriteLine(trie.Add("hui"));
+        Console.WriteLine(trie.Add("uidfdeee"));
+        Console.WriteLine(trie.Contains("hui"));
+        Console.WriteLine(trie.Contains("hui1"));
+        Console.WriteLine(trie.Size);
+        Console.WriteLine(trie.HowManyStartsWithPrefix("h"));
+        Console.WriteLine(trie.HowManyStartsWithPrefix("u"));
+        Console.WriteLine(trie.HowManyStartsWithPrefix("4"));
+        Console.WriteLine(trie.Remove("dsde"));
+        Console.WriteLine(trie.Remove("hui"));
+        Console.WriteLine(trie.Remove("hui"));
+        Console.WriteLine(trie.HowManyStartsWithPrefix("h"));
+        Console.WriteLine(trie.Size);
     }
 }
