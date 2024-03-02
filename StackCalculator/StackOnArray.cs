ad7d7bce1883e3fe3ac3b30e5162d3fe5b0fbc86
ilia Sotnikov <ilia12st@yandex.ru>
2024-03-02 19:15:12 +0300

    redo stack and tests

---
+++ StackCalculator/StackOnArray.cs
@@ -0,0 +1,46 @@
+﻿/// <summary>
+/// Implementation of a stack using an array.
+/// </summary>
+/// <typeparam name="T">The type of object that the stack will contain.</typeparam>
+public class StackOnArray<T> : IStack<T>
+{
+    private int size;
+    private T[] array;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="StackOnArray{T}"/> class.
+    /// </summary>
+    public StackOnArray()
+    {
+        this.size = 0;
+        this.array = new T[32];
+    }
+
+    /// <inheritdoc/>
+    public int Size { get; private set; }
+
+    /// <inheritdoc/>
+    public void Push(T value)
+    {
+        if (this.size >= this.array.Length)
+        {
+            Array.Resize(ref this.array, this.size * 2);
+        }
+
+        this.array[this.size] = value;
+        this.size++;
+    }
+
+    /// <inheritdoc/>
+    public T Pop()
+    {
+        if (this.size == 0)
+        {
+            throw new InvalidOperationException("Stack is empty");
+        }
+
+        this.size--;
+        T returningValue = this.array[this.size];
+        return returningValue;
+    }
+}
--- StackCalculatorTests/StackCalculatorTests.cs
+++ StackCalculatorTests/StackCalculatorTests.cs
@@ -1,7 +1,3 @@
-using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-
 [TestFixture]
 public class StackCalculatorTests
 {
@@ -11,8 +7,8 @@ public class StackCalculatorTests
     [TestCase("10 2 /", 5)]
     public void Calculate_ValidExpressions_ReturnsCorrectResult(string expression, float expected)
     {
-        float actual = StackCalculator.Calculate(expression);
-        float actual2 = StackCalculator.Calculate(expression, "array");
+        float actual = StackCalculator.Calculate(expression, new StackOnList<float>());
+        float actual2 = StackCalculator.Calculate(expression, new StackOnArray<float>());
 
         Assert.That(actual, Is.EqualTo(expected));
         Assert.That(actual2, Is.EqualTo(expected));
@@ -21,8 +17,8 @@ public class StackCalculatorTests
     [TestCase("5 0 /")]
     public void Calculate_DivisionByZero_ThrowsException(string expression)
     {
-        Assert.Throws<DivideByZeroException>(() => StackCalculator.Calculate(expression));
-        Assert.Throws<DivideByZeroException>(() => StackCalculator.Calculate(expression, "array"));
+        Assert.Throws<DivideByZeroException>(() => StackCalculator.Calculate(expression, new StackOnList<float>()));
+        Assert.Throws<DivideByZeroException>(() => StackCalculator.Calculate(expression, new StackOnArray<float>()));
     }
 
     [TestCase("5 2 #")]
@@ -31,9 +27,7 @@ public class StackCalculatorTests
     [TestCase("")]
     public void Calculate_InvalidExpressions_ThrowsException(string expression)
     {
-        Assert.Throws<ArgumentException>(() => StackCalculator.Calculate(expression));
-        Assert.Throws<ArgumentException>(() => StackCalculator.Calculate(expression, "array"));
+        Assert.Throws<ArgumentException>(() => StackCalculator.Calculate(expression, new StackOnList<float>()));
+        Assert.Throws<ArgumentException>(() => StackCalculator.Calculate(expression, new StackOnArray<float>()));
     }
-
-
 }
