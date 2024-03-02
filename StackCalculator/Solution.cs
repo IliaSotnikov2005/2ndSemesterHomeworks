2f9696a2b60a6d1d1959a59f13d2ce60ed6c84d0
ilia Sotnikov <ilia12st@yandex.ru>
2024-03-02 17:45:56 +0300

    rewrite tests

--- StackCalculator/Solution.cs
+++ StackCalculator/Solution.cs
@@ -8,31 +8,15 @@ public class Solution
     /// </summary>
     public static void Main()
     {
+        if (!StackCalculatorTests.Test())
+        {
+            Console.WriteLine("Tests didn't passed.");
+            return;
+        }
+
         Console.Write("Enter the expression: ");
         string expression = Console.ReadLine() ?? string.Empty;
 
         Console.WriteLine($"\nThe result: {StackCalculator.Calculate(expression)}");
-    }
-
-    private static bool Test()
-    {
-        string[] inputs = { "1 2 3 4 + + -", "6 216 3 8 4 + * / -", "1 0 /" };
-        float[] expected = { -8, 0, 0 };
-        bool[] results = new bool[inputs.Length];
-        for (int i = 0; i < inputs.Length; ++i)
-        {
-            try
-            {
-                float result = StackCalculator.Calculate(inputs[i]);
-
-                results[i] = Math.Abs(result - expected[i]) <= float.Epsilon;
-            }
-            catch (DivideByZeroException)
-            {
-                results[i] = i == 2;
-            }
-        }
-
-        return results.All(x => x);
     }
 }
---
+++ StackCalculator/StackCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿/// <summary>
+/// Stack calculator tests class.
+/// </summary>
+public static class StackCalculatorTests
+{
+    /// <summary>
+    /// Stack calculator test.
+    /// </summary>
+    /// <returns>True is all tests passed, else false.</returns>
+    public static bool Test()
+    {
+        string[] inputs = { "1 2 3 4 + + -", "6 216 3 8 4 + * / -", "1 0 /" };
+        float[] expected = { -8, 0, 0 };
+        bool[] results = new bool[inputs.Length];
+        for (int i = 0; i < inputs.Length; ++i)
+        {
+            try
+            {
+                float result = StackCalculator.Calculate(inputs[i]);
+
+                results[i] = Math.Abs(result - expected[i]) <= Math.Abs(result * 0.000001f);
+            }
+            catch (DivideByZeroException)
+            {
+                results[i] = i == 2;
+            }
+        }
+
+        return results.All(x => x);
+    }
+}
