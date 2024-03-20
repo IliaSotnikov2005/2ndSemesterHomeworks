a4843d048a8c17e1939ba19315d8247df38d6155
Ilya Sotnikov <ilia12st@yandex.ru>
2024-03-21 02:26:24 +0300

    make full working parse tree

--- ParseTree/ParseTree/ParseTree.cs
+++ ParseTree/ParseTree/ParseTree.cs
@@ -1,76 +1,125 @@
-using System.Numerics;
 using System.Text;
 
 namespace ParseTreeSpace
 {
-    public class ParseTree
+    public class ParseTree(IEdgeContent content) // TODO change public -> private
     {
-        public Edge? root = null; // TODO change public -> private readonly
+        public IEdgeContent Content { get; set; } = content;
+        public ParseTree? LeftChild { get; set; } = null;
+        public ParseTree? RightChild { get; set; } = null;
 
-        public class Edge(IEdgeContent content) // TODO change public -> private
+        public Operand Evaluate()
         {
-            public IEdgeContent Content { get; set; } = content;
-            public Edge? LeftChild { get; set; } = null;
-            public Edge? RightChild {get; set; } = null;
-
-            public Operand Evaluate()
+            if (Content is Operand operand)
             {
-                if (Content is Operand operand)
-                {
-                    return operand;
-                }
-                else if (Content is IOperator op)
-                {
-                    Operand leftValue = LeftChild?.Evaluate() ?? new Operand(0);
-                    Operand rightValue = RightChild?.Evaluate() ?? new Operand(0);
-
-                    return op.Calculate(leftValue, rightValue);
-                }
-                else
-                {
-                    throw new ArgumentException();
-                }
+                return operand;
             }
-
-            public override string ToString()
+            else if (Content is IOperator op)
             {
-                return Content.ToString();
-            }
+                Operand leftValue = LeftChild?.Evaluate() ?? new Operand(0);
+                Operand rightValue = RightChild?.Evaluate() ?? new Operand(0);
 
-            public void Print()
+                return op.Calculate(leftValue, rightValue);
+            }
+            else
             {
-                if (this.LeftChild == null)
-                {
-                    Console.Write($" {this.ToString()} ");
-                }
-                else
-                {
-                    Console.Write($"( {this.ToString()} ");
-                    this.LeftChild.Print();
-                    this.RightChild.Print();
-                    Console.Write(")");
-                }
+                throw new ArgumentException();
             }
-        }
-
-        public void BuildFromFile()
-        {
-        
-        }
-
-        public void Print()
-        {
-            root.Print();
         }
 
         public override string ToString()
         {
-            return "";
+            var sb = new StringBuilder();
+            Stack<ParseTree> stack = new Stack<ParseTree>();
+            stack.Push(this);
+
+            while (stack.Count > 0)
+            {
+                ParseTree current = stack.Pop();
+
+                if (current.LeftChild == null || current.RightChild == null)
+                {
+                    sb.Append($" {current.Content} ");
+                }
+                else
+                {
+                    sb.Append($"( {current.Content} ");
+                    stack.Push(current.RightChild);
+                    stack.Push(current.LeftChild);
+                }
+                if ((stack.Count - 1) % 2 == 0 && current.Content is not IOperator)
+                {
+                    sb.Append(")");
+                }
+            }
+
+            sb.Append(")");
+            return sb.ToString();
         }
 
-        private void BuildTree(string expression)
+        private static ParseTree? buildTree(ref string[] expression)
         {
+            if (expression.Length == 0)
+            {
+                return null;
+            }
 
+            string element = expression[0];
+            expression = expression[1..];
+            if (int.TryParse(element, out int number))
+            {
+                return new ParseTree(new Operand(number));
+            }
+
+            switch (element)
+            {
+                case "+":
+                    {
+                        ParseTree edge = new(new Plus());
+                        edge.LeftChild = buildTree(ref expression);
+                        edge.RightChild = buildTree(ref expression);
+                        return edge;
+                    }
+                case "-":
+                    {
+                        ParseTree edge = new(new Minus());
+                        edge.LeftChild = buildTree(ref expression);
+                        edge.RightChild = buildTree(ref expression);
+                        return edge;
+                    }
+                case "*":
+                    {
+                        ParseTree edge = new(new Multiplication());
+                        edge.LeftChild = buildTree(ref expression);
+                        edge.RightChild = buildTree(ref expression);
+                        return edge;
+                    }
+                case "/":
+                    {
+                        ParseTree edge = new(new Division());
+                        edge.LeftChild = buildTree(ref expression);
+                        edge.RightChild = buildTree(ref expression);
+                        return edge;
+                    }
+                default:
+                    {
+                        throw new ArgumentException();
+                    }
+            }
+        }
+
+        public static ParseTree buildTreeFromFile(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                throw new FileNotFoundException("File with expression is not found.");
+            }
+            string input = File.ReadAllText(filePath);
+            input = input.Replace("( ", "").Replace(") ", "")[..^2];
+            string[] expression = input.Split(" ");
+
+            ParseTree? root = buildTree(ref expression) ?? throw new ArgumentException();
+            return root;
         }
     }
 }
--- ParseTree/ParseTree/Program.cs
+++ ParseTree/ParseTree/Program.cs
@@ -1,9 +1,5 @@
-﻿// Console.WriteLine("Enter the path to file with tree: ");
-// string filePath = Console.ReadLine()?? string.Empty;
-using ParseTreeSpace;
+﻿using ParseTreeSpace;
 
-ParseTree tree = new();
-tree.root = new ParseTree.Edge(new Plus());
-tree.root.LeftChild = new ParseTree.Edge(new Operand(1));
-tree.root.RightChild = new ParseTree.Edge(new Operand(3));
-tree.Print();
+ParseTree tree = ParseTree.buildTreeFromFile("expression.txt");
+Console.WriteLine(tree.Evaluate());
+Console.WriteLine(tree);
