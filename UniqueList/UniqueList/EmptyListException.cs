ff192532199556676d97bae95354228c77bdc640
Ilya Sotnikov <ilia12st@yandex.ru>
2024-03-21 19:37:43 +0300

    add documentation

---
+++ UniqueList/UniqueList/EmptyListException.cs
@@ -0,0 +1,10 @@
+// <copyright file="EmptyListException.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+/// <summary>
+/// Empty list exception class.
+/// </summary>
+public class EmptyListException : Exception
+{
+}
