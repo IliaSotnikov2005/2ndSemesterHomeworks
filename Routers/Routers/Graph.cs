4e83bbced713a6ee90ea418c92d68f4520fadfd9
Ilya Sotnikov <ilia12st@yandex.ru>
2024-03-30 22:18:19 +0300

    add documentation

--- Routers/Routers/Graph.cs
+++ Routers/Routers/Graph.cs
@@ -1,11 +1,30 @@
+// <copyright file="Graph.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace RoutersGraph;
+
 using System.Text;
 
+/// <summary>
+/// Graph class.
+/// </summary>
 public class Graph
 {
+    /// <summary>
+    /// Gets the set of vertices in the graph.
+    /// </summary>
     public HashSet<Vertex> Vertices { get; private set; } = [];
 
+    /// <summary>
+    /// Gets the list of edges in the graph.
+    /// </summary>
     public List<Edge> Edges { get; private set; } = [];
 
+    /// <summary>
+    /// Builds the graph from the topology defined in the specified file.
+    /// </summary>
+    /// <param name="filename">The name of the file containing the topology.</param>
     public void BuildGraphFromTopology(string filename)
     {
         if (!File.Exists(filename))
@@ -17,52 +36,22 @@ public class Graph
 
         foreach (var input in topology)
         {
-            ParseInput(input);
+            this.ParseInput(input);
         }
     }
 
+    /// <summary>
+    /// Adds an edge to the graph.
+    /// </summary>
+    /// <param name="edge">The edge to add.</param>
     public void AddEdge(Edge edge)
     {
-        Edges.Add(edge);
-        Vertices.Add(edge.Vertex1);
-        Vertices.Add(edge.Vertex2);
+        this.Edges.Add(edge);
+        this.Vertices.Add(edge.Vertex1);
+        this.Vertices.Add(edge.Vertex2);
     }
 
-    private void ParseInput(string input)
-    {
-        string[] parts = input.Split(':');
-        var router1 = new Vertex(int.Parse(parts[0]));
-        Vertices.Add(router1);
-
-
-        string[] connections = parts[1].Split(',');
-
-        foreach (string connection in connections)
-        {
-            string[] routerAndBandwidth = connection.Trim().Split(' ');
-            var router2 = new Vertex(int.Parse(routerAndBandwidth[0]));
-            Vertices.Add(router2);
-
-            int bandwidth = int.Parse(routerAndBandwidth[1].Trim('(', ')'));
-
-            var edge = new Edge(router1, router2, bandwidth);
-            Edges.Add(edge);
-        }
-    }
-
-    public record Vertex(int router)
-    {
-        public int Router { get; set; } = router;
-    }
-
-    public record Edge(Vertex vertex1, Vertex vertex2, int bandwidth)
-    {
-        public Vertex Vertex1 { get; set; } = vertex1;
-        public Vertex Vertex2 { get; set; } = vertex2;
-
-        public int Bandwidth { get; set; } = bandwidth;
-    }
-
+    /// <inheritdoc/>
     public override string ToString()
     {
         var sb = new StringBuilder();
@@ -90,5 +79,58 @@ public class Graph
         }
 
         return sb.ToString();
+    }
+
+    /// <summary>
+    /// Represents a vertex in the graph.
+    /// </summary>
+    public record Vertex(int router)
+    {
+        /// <summary>
+        /// Gets the ID of the router.
+        /// </summary>
+        public int Router { get; private set; } = router;
+    }
+
+    /// <summary>
+    /// Represents an edge in the graph.
+    /// </summary>
+    public record Edge(Vertex vertex1, Vertex vertex2, int bandwidth)
+    {
+        /// <summary>
+        /// Gets the first vertex in the edge.
+        /// </summary>
+        public Vertex Vertex1 { get; private set; } = vertex1;
+
+        /// <summary>
+        /// Gets the second vertex in the edge.
+        /// </summary>
+        public Vertex Vertex2 { get; private set; } = vertex2;
+
+        /// <summary>
+        /// Gets the bandwidth of the edge.
+        /// </summary>
+        public int Bandwidth { get; private set; } = bandwidth;
+    }
+
+    private void ParseInput(string input)
+    {
+        string[] parts = input.Split(':');
+        var router1 = new Vertex(int.Parse(parts[0]));
+        this.Vertices.Add(router1);
+
+        string[] connections = parts[1].Split(',');
+
+        foreach (string connection in connections)
+        {
+            string[] routerAndBandwidth = connection.Trim().Split(' ');
+            var router2 = new Vertex(int.Parse(routerAndBandwidth[0]));
+            this.Vertices.Add(router2);
+
+            int bandwidth = int.Parse(routerAndBandwidth[1].Trim('(', ')'));
+
+            var edge = new Edge(router1, router2, bandwidth);
+            this.Edges.Add(edge);
+        }
     }
 }
--- Routers/Routers/Program.cs
+++ Routers/Routers/Program.cs
@@ -1,6 +1,24 @@
-﻿Graph graph = new Graph();
-graph.BuildGraphFromTopology("text.txt");
-Console.WriteLine($"Input topology:\n{graph}");
+﻿// <copyright file="Program.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
 
-var newGraph = KruskalsAlgorithm.Run(graph);
-Console.WriteLine($"New topology:\n{newGraph}");
+namespace RoutersGraph;
+
+/// <summary>
+/// Program class.
+/// </summary>
+public static class Program
+{
+    /// <summary>
+    /// Entry point for program.
+    /// </summary>
+    public static void Main()
+    {
+        Graph graph = new Graph();
+        graph.BuildGraphFromTopology("text.txt");
+        Console.WriteLine($"Input topology:\n{graph}");
+
+        var newGraph = KruskalsAlgorithm.Run(graph);
+        Console.WriteLine($"New topology:\n{newGraph}");
+    }
+}
