97635ade6fcac6d33e841c088e37dcc6c32d7e8e
Ilya Sotnikov <ilia12st@yandex.ru>
2024-03-30 22:18:19 +0300

    add documentation

--- Routers/Routers/KruskalsAlgorithm.cs
+++ Routers/Routers/KruskalsAlgorithm.cs
@@ -1,9 +1,23 @@
+// <copyright file="KruskalsAlgorithm.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace RoutersGraph;
+
+/// <summary>
+/// Kruskal's algorithm class.
+/// </summary>
 public static class KruskalsAlgorithm
 {
+    /// <summary>
+    /// Runs the Kruskal's algorithm on the given graph.
+    /// </summary>
+    /// <param name="graph">The input graph.</param>
+    /// <returns>The maximum spanning tree of the input graph.</returns>
     public static Graph Run(Graph graph)
     {
         var sortedEdges = graph.Edges.OrderByDescending(e => e.Bandwidth).ToList();
-        
+
         var resultGraph = new Graph();
 
         foreach (var edge in sortedEdges)
