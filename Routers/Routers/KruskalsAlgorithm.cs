8546db389eb854fdcf81028c9bafff2cc00ac9ed
Ilya Sotnikov <ilia12st@yandex.ru>
2024-03-30 21:54:52 +0300

    write the graph output to the screen, redo the graph, write a kruskals

--- Routers/Routers/KruskalsAlgorithm.cs
+++ Routers/Routers/KruskalsAlgorithm.cs
@@ -2,8 +2,20 @@ public static class KruskalsAlgorithm
 {
     public static Graph Run(Graph graph)
     {
-        var sortedEdges = graph.Edges.OrderByDescending(e => e.bandwidth).ToList();
+        var sortedEdges = graph.Edges.OrderByDescending(e => e.Bandwidth).ToList();
         
-        return graph;
+        var resultGraph = new Graph();
+
+        foreach (var edge in sortedEdges)
+        {
+            if (resultGraph.Vertices.Contains(edge.Vertex1) && resultGraph.Vertices.Contains(edge.Vertex2))
+            {
+                continue;
+            }
+
+            resultGraph.AddEdge(edge);
+        }
+
+        return resultGraph;
     }
 }
