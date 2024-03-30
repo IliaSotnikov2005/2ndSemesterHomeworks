f24f4af7c3c6c1df7a0c1abd18480032fe76ac66
Ilya Sotnikov <ilia12st@yandex.ru>
2024-03-30 23:07:22 +0300

    add tests

---
+++ Routers/Routers/FileIsEmptyException.cs
@@ -0,0 +1,10 @@
+// <copyright file="FileIsEmptyException.cs" company="PlaceholderCompany">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+/// <summary>
+/// File is empty exception.
+/// </summary>
+public class FileIsEmptyException : Exception
+{
+}
