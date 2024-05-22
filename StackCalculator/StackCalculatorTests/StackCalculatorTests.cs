d515e30217f28bfb763ac6c411fe4ddfab6dbb40
Ilya Sotnikov <ilia12st@yandex.ru>
2024-05-22 23:29:19 +0300

    fix code duplication

--- StackCalculator/StackCalculatorTests/StackCalculatorTests.cs
+++ StackCalculator/StackCalculatorTests/StackCalculatorTests.cs
@@ -1,34 +1,54 @@
 [TestFixture]
 public class StackCalculatorTests
 {
-    [TestCase("3 4 +", 7)]
-    [TestCase("5 2 -", 3)]
-    [TestCase("-4 6 *", -24)]
-    [TestCase("10 2 /", 5)]
-    [TestCase("8 84 21 8 1 1 2 3 * - + + * / -", 7)]
-    public void Calculate_ValidExpressions_ReturnsCorrectResult(string expression, float expected)
+    [TestCaseSource(nameof(StackCalculatorTestData))]
+    public void Calculate_ValidExpressions_ReturnsCorrectResult(IStack<float> stack)
     {
-        float actual = StackCalculator.Calculate(expression, new StackOnList<float>());
-        float actual2 = StackCalculator.Calculate(expression, new StackOnArray<float>());
+        string[] inputs =
+        [
+        "3 4 +",
+        "5 2 -",
+        "-4 6 *",
+        "10 2 /",
+        "8 84 21 8 1 1 2 3 * - + + * / -",
+        ];
+        int[] expected = [7, 3, -24, 5];
 
-        Assert.That(actual, Is.EqualTo(expected));
-        Assert.That(actual2, Is.EqualTo(expected));
+        for (int i = 0; i < expected.Length; ++i)
+        {
+            float actual = StackCalculator.Calculate(inputs[i], stack);
+
+            Assert.That(actual, Is.EqualTo(expected[i]));
+        }
     }
 
-    [TestCase("5 0 /")]
-    public void Calculate_DivisionByZero_ThrowsException(string expression)
+    [TestCaseSource(nameof(StackCalculatorTestData))]
+    public void Calculate_DivisionByZero_ThrowsException(IStack<float> stack)
     {
-        Assert.Throws<DivideByZeroException>(() => StackCalculator.Calculate(expression, new StackOnList<float>()));
-        Assert.Throws<DivideByZeroException>(() => StackCalculator.Calculate(expression, new StackOnArray<float>()));
+        string input = "5 0 /";
+
+        Assert.Throws<DivideByZeroException>(() => StackCalculator.Calculate(input, stack));
     }
 
-    [TestCase("5 2 #")]
-    [TestCase("3 2 t 3 + + +")]
-    [TestCase("3 3 3 -")]
-    [TestCase("")]
-    public void Calculate_InvalidExpressions_ThrowsException(string expression)
+    [TestCaseSource(nameof(StackCalculatorTestData))]
+    public void Calculate_InvalidExpressions_ThrowsException(IStack<float> stack)
     {
-        Assert.Throws<ArgumentException>(() => StackCalculator.Calculate(expression, new StackOnList<float>()));
-        Assert.Throws<ArgumentException>(() => StackCalculator.Calculate(expression, new StackOnArray<float>()));
+        string[] inputs =
+        [
+        "5 2 #",
+        "3 2 t 3 + + +",
+        "3 3 3 -",
+        "",
+        ];
+        for (int i = 0; i < inputs.Length; ++i)
+        {
+            Assert.Throws<ArgumentException>(() => StackCalculator.Calculate(inputs[i], stack));
+        }
+    }
+
+    private static IEnumerable<TestCaseData> StackCalculatorTestData()
+    {
+        yield return new TestCaseData(new StackOnList<float>());
+        yield return new TestCaseData(new StackOnArray<float>());
     }
 }
