a8ef33d59c94b405e3dff495cc02c096b128e062
ilia Sotnikov <ilia12st@yandex.ru>
2024-05-23 16:56:41 +0300

    upload

---
+++ RunningButton/RunningButton/Program.cs
@@ -0,0 +1,26 @@
+﻿// <copyright file="Program.cs" company="IlyaSotnikov">
+// Copyright (c) PlaceholderCompany. All rights reserved.
+// </copyright>
+
+namespace RunningButton
+{
+    using System;
+    using System.Windows.Forms;
+
+    /// <summary>
+    /// The entry point to the program.
+    /// </summary>
+    internal static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        public static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new RunningButton());
+        }
+    }
+}
