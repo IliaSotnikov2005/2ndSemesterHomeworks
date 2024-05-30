ac7098cf7356e3c91a942e7f2f05e4167c57ed0c
Ilya Sotnikov <ilia12st@yandex.ru>
2024-05-30 20:08:39 +0300

    add documentation

--- NullElementsCounter/NullElementsCounter/INullChecker.cs
+++ NullElementsCounter/NullElementsCounter/INullChecker.cs
@@ -4,7 +4,16 @@
 
 namespace NullElementsCounter;
 
+/// <summary>
+/// An interface for objects that specifies "null".
+/// </summary>
+/// <typeparam name="T">Type of object.</typeparam>
 public interface INullChecker<T>
 {
+    /// <summary>
+    /// Determines if element is "null".
+    /// </summary>
+    /// <param name="element">Element.</param>
+    /// <returns>True if element is "null" else false.</returns>
     public bool IsNull(T element);
 }
